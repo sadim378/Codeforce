7078f016d22495c0e58c7f3fed1aac941f9c5240
Md. Abu Taleb Sadim <sadimakon3@gmail.com>
2026-07-07 10:41:47 +0600

    Add files via upload

---
+++ 677A.py
@@ -0,0 +1,13 @@
+n, h = map(int, input().split())
+
+heights = list(map(int, input().split()))
+
+width = 0
+
+for height in heights:
+    if height <= h:
+        width += 1
+    else:
+        width += 2
+
+print(width)
