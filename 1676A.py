00f9aa6c45d4ddf129a4a3615e45bf07e6e3d782
Md. Abu Taleb Sadim <sadimakon3@gmail.com>
2026-07-30 16:56:49 +0600

    Add files via upload

---
+++ 1676A.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(t):
+    ticket = input()
+
+    first_sum = int(ticket[0]) + int(ticket[1]) + int(ticket[2])
+    second_sum = int(ticket[3]) + int(ticket[4]) + int(ticket[5])
+
+    if first_sum == second_sum:
+        print("YES")
+    else:
+        print("NO")
